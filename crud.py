51826efdf053b6abb9324ab8c42104235cd94820
khataev <ruslanhataevEL1801@yandex.ru>
2026-06-26 06:48:13 +0300

    Начал изучать валидацию pydantic

--- crud.py
+++ crud.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Body
+from pydantic import BaseModel, Field
+
+class Message(BaseModel):
+    id: int
+    message: str
+
+
 
 app = FastAPI()
 
 
-messages_db = {"0": "First post in FastAPI"}
+messages_db = []
 
 
 @app.get("/")
@@ -16,21 +23,25 @@ async def get_message(message_id: str) -> str:
     return messages_db[message_id]
 
 
-@app.post("/message")
-async def create_message(message: str) -> str:
-    pass
+@app.post("/message", status_code=status.HTTP_201_CREATED)
+async def create_message(message: str = Body()) -> str:
+    current_index = str(len(messages_db))
+    messages_db[current_index] = message
+    return f"Message created!"
 
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str) -> str:
-    pass
+async def update_message(message_id: str, message: str = Body()) -> str:
+    messages_db[message_id] = message
+    return f"Message updated!"
 
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: str) -> str:
-    pass
-
+    messages_db.pop(message_id)
+    return f"Message ID={message_id} deleted!"
 
 @app.delete("/")
 async def kill_message_all() -> str:
-    pass
+    messages_db.clear()
+    return "All messages deleted!"
