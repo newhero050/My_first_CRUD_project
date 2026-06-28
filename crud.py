7875bed6aa6d9aebfdc8634a3c2d661f29f27062
newhero050 <ruslanhataevEL1801@yandex.ru>
2026-06-28 06:11:05 +0300

    Изучение модели pydanctic, обработка ошибок HTTPException

--- crud.py
+++ crud.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, status, Body
 from pydantic import BaseModel, Field
+from typing import List
+from fastapi import HTTPException
+
 
 class Message(BaseModel):
-    id: int
-    message: str
+    id: int = None
+    text: str
 
+    model_config = {
+        "json_schema_extra": {
+            "examples":
+                [
+                    {
+                        "text": "Simple message",
+                    }
+                ]
+        }
+    }
 
 
 app = FastAPI()
@@ -14,32 +27,46 @@ messages_db = []
 
 
 @app.get("/")
-async def get_all_messages() -> dict:
+async def get_all_messages() -> List[Message]:
     return messages_db
 
 
 @app.get("/message/{message_id}")
-async def get_message(message_id: str) -> str:
-    return messages_db[message_id]
+async def get_message(message_id: int) -> Message:
+    try:
+        return messages_db[message_id]
+    except IndexError:
+        raise HTTPException(status_code=404, detail='Message not found')
 
 
 @app.post("/message", status_code=status.HTTP_201_CREATED)
-async def create_message(message: str = Body()) -> str:
-    current_index = str(len(messages_db))
-    messages_db[current_index] = message
-    return f"Message created!"
+async def create_message(message: Message) -> str:
+    if len(messages_db) == 0:
+        message.id = 0
+    else:
+        message.id = max([i.dict()['id'] for i in messages_db]) + 1
+    messages_db.append(message)
+    return f'Message created!'
 
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str = Body()) -> str:
-    messages_db[message_id] = message
-    return f"Message updated!"
+async def update_message(message_id: int, message: str = Body()) -> str:
+    try:
+        edit_message = messages_db[message_id]
+        edit_message.text = message
+        return f"Message updated!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail='Message not found')
 
 
 @app.delete("/message/{message_id}")
-async def delete_message(message_id: str) -> str:
-    messages_db.pop(message_id)
-    return f"Message ID={message_id} deleted!"
+async def delete_message(message_id: int) -> str:
+    try:
+        messages_db.pop(message_id)
+        return f"Message ID={message_id} deleted!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail='Message not found')
+
 
 @app.delete("/")
 async def kill_message_all() -> str:
