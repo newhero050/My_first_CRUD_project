428637ff6080bd573d17b0307e088827111ab72a
newhero050 <ruslanhataevEL1801@yandex.ru>
2026-07-05 19:43:14 +0300

    Изучаю Jinja, принятие данных из форм

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Body, Request
+from fastapi import FastAPI, status, Body, Request, Form
 from pydantic import BaseModel, Field
 from typing import List
 from fastapi import HTTPException
@@ -29,24 +29,26 @@ messages_db = []
 
 @app.get("/")
 async def get_all_messages(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("message.html", {request: request})
+    return templates.TemplateResponse("message.html",
+                                      {"request": request, "messages": messages_db})
 
 
 @app.get("/message/{message_id}")
-async def get_message(message_id: int) -> Message:
+async def get_message(request: Request, message_id: int) -> HTMLResponse:
     try:
-        return messages_db[message_id]
+        return templates.TemplateResponse("message.html",
+                                          {"request": request, "message": messages_db[message_id]})
     except IndexError:
         raise HTTPException(status_code=404, detail='Message not found')
 
 
 @app.post("/message", status_code=status.HTTP_201_CREATED)
-async def create_message(message: Message) -> str:
+async def create_message(request: Request, message: str = Form()) ->  HTMLResponse:
     if len(messages_db) == 0:
-        message.id = 0
+        max_id_message = 0
     else:
-        message.id = max([i.dict()['id'] for i in messages_db]) + 1
-    messages_db.append(message)
+        max_id_message = max([i.dict()['id'] for i in messages_db]) + 1
+    messages_db.append(Message(id=max_id_message))
     return f'Message created!'
 
 
