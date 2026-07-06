874619ee15cc8c3a97dddf7c41d63073a078b26d
newhero050 <ruslanhataevEL1801@yandex.ru>
2026-07-06 12:03:14 +0300

    Изучаю jinja, вытаскиваем данные из форм ура

--- crud.py
+++ crud.py
@@ -27,33 +27,32 @@ templates = Jinja2Templates(directory="templates")
 
 messages_db = []
 
+
 @app.get("/")
-async def get_all_messages(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("message.html",
-                                      {"request": request, "messages": messages_db})
+def get_all_messages(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("message.html", {"request": request, "messages": messages_db})
 
 
-@app.get("/message/{message_id}")
-async def get_message(request: Request, message_id: int) -> HTMLResponse:
+@app.get(path="/message/{message_id}")
+def get_message(request: Request, message_id: int) -> HTMLResponse:
     try:
-        return templates.TemplateResponse("message.html",
-                                          {"request": request, "message": messages_db[message_id]})
+        return templates.TemplateResponse("message.html", {"request": request, "message": messages_db[message_id]})
     except IndexError:
-        raise HTTPException(status_code=404, detail='Message not found')
+        raise HTTPException(status_code=404, detail="Message not found")
 
 
-@app.post("/message", status_code=status.HTTP_201_CREATED)
+@app.post("/", status_code=status.HTTP_201_CREATED)
 async def create_message(request: Request, message: str = Form()) ->  HTMLResponse:
     if len(messages_db) == 0:
         max_id_message = 0
     else:
         max_id_message = max([i.dict()['id'] for i in messages_db]) + 1
-    messages_db.append(Message(id=max_id_message))
-    return f'Message created!'
+    messages_db.append(Message(id=max_id_message, text=message))
+    return templates.TemplateResponse("message.html", {"request": request, "messages": messages_db})
 
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: int, message: str = Body()) -> str:
+async def update_message(message_id: int, message: str = Body()) -> HTMLResponse:
     try:
         edit_message = messages_db[message_id]
         edit_message.text = message
